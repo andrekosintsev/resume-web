746c41a91c29c04efd0f7874ac18b70ec5362d7b
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-09-01 17:10:22 +0200

    images added

--- app.js
+++ app.js
@@ -139,11 +139,12 @@ let usercard = document.getElementById("usercard");
 let allinfo = document.getElementById("allinfo");
 
 let p = document.createElement("p");
+let s = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
-p.innerText = `${tg.initDataUnsafe} ${tg.initDataUnsafe.user}`;
+s.innerText = `${tg.initDataUnsafe} ${tg.initDataUnsafe.user}`;
 console.log(${tg.initDataUnsafe} ${tg.initDataUnsafe.user});
 
 usercard.appendChild(p);
 
-allinfo.appendChild();
+allinfo.appendChild(s);
