dc35ae673c230d3b20375de05dd06c35521e6059
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-09-28 20:18:15 +0200

    work changed

--- app_work.js
+++ app_work.js
@@ -43,8 +43,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function() {
     if (validateInput(['name', 'industry', 'position', 'startDate', 'endDate'])) {
             return;
     }
-    const jobs = [];
-        jobs.push({
+    const jobs = [{
             name: document.getElementById('name').value,
             address: document.getElementById('address').value,
             website: document.getElementById('website').value,
@@ -55,7 +54,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function() {
             summary: document.getElementById('summary').value,
             highlights: document.getElementById('highlights').value,
             keywords: document.getElementById('keywords').value
-        });
+        }];
     tg.sendData(JSON.stringify({
         works: jobs
     }));
