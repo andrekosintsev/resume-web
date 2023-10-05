83ac40653bea4e8dea43b159dbc1342f97fc422f
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-10-05 12:42:11 +0200

    publication added

--- app_publication.js
+++ app_publication.js
@@ -2,9 +2,9 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#229ED9';
-tg.MainButton.setText("Add to List");
+tg.MainButton.setText("Save");
+
+tg.MainButton.show();
 
 
 function getQueryParam(name) {
@@ -14,142 +14,99 @@ function getQueryParam(name) {
 
 const encodedJsonData = getQueryParam("json_data");
 
-let publicationsForm = document.getElementById('publications_form');
-let publicationsList = document.getElementById('publications_list');
+let deleteButton = document.getElementById("delete-button");
+var maxCharacters = 1000;
 
-[document.getElementById('name'),
-    document.getElementById('publisher'),
-    document.getElementById('releaseDate'),
-    document.getElementById('url'),
-    document.getElementById('summary')
-].forEach(item => {
-    item.addEventListener('mouseover', function() {
-        tg.MainButton.color = '#229ED9';
-        tg.MainButton.setText("Add to List");
-        tg.MainButton.hide();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
-        }
-    });
+let summaryText = new SimpleMDE({
+            element: document.getElementById("summary"),
+            spellChecker: false, // Enable spell checker if desired
+            toolbar: [
+                "bold",           // Bold text
+                "italic",         // Italic text
+                "heading",        // Headings (h1, h2, h3, etc.)
+                "|",              // Separator
+                "unordered-list", // Unordered list (bullets)
+                "ordered-list",   // Ordered list (numbers)
+                "|",              // Separator
+                "preview"        // Toggle preview mode
+            ]
+        });
+[summaryText].forEach(item => {
+            item.codemirror.on("change", function () {
+                        var currentText = item.value();
+                        var currentLength = currentText.length;
+
+                        if (currentLength > maxCharacters) {
+                            currentText = currentText.substring(0, maxCharacters);
+                            item.value(currentText);
+                        }
+                    });
 });
+
 
 if (encodedJsonData) {
     const jsonData = decodeURIComponent(encodedJsonData);
     const jsonObject = JSON.parse(jsonData);
-    jsonObject.forEach(item => {
-        addPublicationEntry(item);
-    });
+    populateFormForEditing(jsonObject);
+    toggleDeleteButton(true);
+} else {
+    toggleDeleteButton(false);
 }
 
-// Function to generate and append publication entry HTML
-function addPublicationEntry(entry) {
-    const listItem = document.createElement('li');
-    listItem.className = 'list-group-item justify-content-between align-items-center';
-
-    const entryHTML = `
-            <strong>Name:</strong> <i>${entry.name}</i><br>
-            <strong>Publisher:</strong> <i>${entry.publisher}</i><br>
-            <strong>Release Date:</strong> <i>${entry.releaseDate}</i><br>
-            <strong>URL:</strong> <a href="${entry.url}" target="_blank"><i>${entry.url}</i></a><br>
-            <strong>Summary:</strong> <i>${entry.summary}</i><br>
-        `;
-
-    listItem.innerHTML = entryHTML;
-    // Edit button event listener
-    const editButton = document.createElement('button');
-    editButton.className = 'btn btn-info btn-sm me-2';
-    editButton.textContent = 'Edit';
-
-    editButton.addEventListener('click', () => {
-        populateFormForEditing(entry);
-        listItem.remove();
-        tg.MainButton.color = '#229ED9';
-        tg.MainButton.setText("Save changes");
-        tg.MainButton.hide();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
+function toggleDeleteButton(showButton) {
+        let deleteButton = document.getElementById("delete-button");
+        if (showButton) {
+            deleteButton.style.display = "block"; // Show the button
+        } else {
+            deleteButton.style.display = "none"; // Hide the button
         }
-    });
-
-    // Delete button event listener
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn btn-danger btn-sm me-2';
-    deleteButton.textContent = 'Delete';
-
-    deleteButton.addEventListener('click', () => {
-        listItem.remove();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
-            tg.MainButton.color = '#2CAB37';
-            tg.MainButton.setText("Save");
-        }
-    });
-
-    const div = document.createElement('div');
-    div.appendChild(editButton);
-    div.appendChild(deleteButton);
-    listItem.appendChild(div);
-    publicationsList.appendChild(listItem);
 }
 
 // Function to populate the publication form with data for editing
 function populateFormForEditing(entry) {
-    document.getElementById('name').value = entry.name;
-    document.getElementById('publisher').value = entry.publisher;
-    document.getElementById('releaseDate').value = entry.releaseDate;
-    document.getElementById('url').value = entry.url;
-    document.getElementById('summary').value = entry.summary;
+    document.getElementById('id').value = entry.id || "";
+    document.getElementById('name').value = entry.name || "";
+    document.getElementById('publisher').value = entry.publisher || "";
+    document.getElementById('releaseDate').value = entry.releaseDate || "";
+    document.getElementById('url').value = entry.url || "";
+    summaryText.value(entry.summary || "");
 }
 
-// Edit button event listener
-function editPublicationEntry(entry) {
-    populateFormForEditing(entry);
-}
-
-// Add button click event listener
-function addEntry() {
-    if (validateInput(['name', 'releaseDate'])) {
-        return;
-    }
-    const entryData = {
-        name: document.getElementById('name').value,
-        publisher: document.getElementById('publisher').value,
-        releaseDate: document.getElementById('releaseDate').value,
-        url: document.getElementById('url').value,
-        summary: document.getElementById('summary').value,
-    };
-
-    addPublicationEntry(entryData);
-    publicationsForm.reset();
-};
+deleteButton.addEventListener("click", function () {
+            tg.showPopup({
+                              title: 'Action Delete',
+                              message: 'Are you sure you want to delete this publication?',
+                              buttons: [
+                                  {id: 'delete', type: 'destructive', text: 'Delete anyway'},
+                                  {type: 'cancel'},
+                              ]
+                          }, function(buttonId) {
+                              if (buttonId === 'delete') {
+                                 tg.sendData(JSON.stringify(
+                                 {
+                                     del_element: {
+                                         pub_id: document.getElementById('id').value
+                                     }
+                                 }
+                                 ));
+                                 tg.close();
+                              }
+                          });
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    if (tg.MainButton.text === "Add to List") {
-        addEntry();
-        tg.MainButton.color = '#2CAB37';
-        tg.MainButton.setText("Save");
-        return;
+    if (validateInput(['name', 'releaseDate'])) {
+            return;
     }
-    if (tg.MainButton.text === "Save changes") {
-        addEntry();
-        tg.MainButton.color = '#2CAB37';
-        tg.MainButton.setText("Save");
-        return;
-    }
-    const listItems = publicationsList.querySelectorAll('li');
-    const pubs = [];
-    listItems.forEach((item) => {
-        const strongElements = item.querySelectorAll('i');
-        pubs.push({
-            name: strongElements[0].textContent.trim(),
-            publisher: strongElements[1].textContent.trim(),
-            releaseDate: strongElements[2].textContent.trim(),
-            url: strongElements[3].textContent.trim(),
-            summary: strongElements[4].textContent.trim()
-        });
-    });
     tg.sendData(JSON.stringify({
-        publications: pubs
+        publications: [{
+                id: document.getElementById('id').value,
+                name: document.getElementById('name').value,
+                publisher: document.getElementById('publisher').value,
+                releaseDate: document.getElementById('releaseDate').value,
+                url: document.getElementById('url').value,
+                summary: document.getElementById('summary').value,
+        }]
     }));
     tg.close();
 });
