7b4d5ad05a459791d9ada46353203395d03ccc15
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-10-05 13:46:55 +0200

    skill modified

--- app_skill.js
+++ app_skill.js
@@ -2,9 +2,9 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = '#229ED9';
-tg.MainButton.setText("Add to List");
+tg.MainButton.setText("Save");
+
+tg.MainButton.show();
 
 function getQueryParam(name) {
     const urlSearchParams = new URLSearchParams(window.location.search);
@@ -13,132 +13,68 @@ function getQueryParam(name) {
 
 const encodedJsonData = getQueryParam("json_data");
 
-const skillsForm = document.getElementById('skills_form');
-const skillsList = document.getElementById('skills_list');
-
-[document.getElementById('name'),
-    document.getElementById('level'),
-    document.getElementById('keywords')
-].forEach(item => {
-    item.addEventListener('mouseover', function() {
-        tg.MainButton.color = '#229ED9';
-        tg.MainButton.setText("Add to List");
-        tg.MainButton.hide();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
-        }
-    });
-});
+let deleteButton = document.getElementById("delete-button");
 
 if (encodedJsonData) {
     const jsonData = decodeURIComponent(encodedJsonData);
     const fixedJson = jsonData.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":');
     const jsonObject = JSON.parse(fixedJson);
-    jsonObject.forEach(item => {
-        addSkillEntry(item);
-    });
-}
+     populateFormForEditing(jsonObject);
+        toggleDeleteButton(true);
+    } else {
+        toggleDeleteButton(false);
+    }
 
-function addSkillEntry(entry) {
-    const listItem = document.createElement('li');
-    listItem.className = 'list-group-item justify-content-between align-items-center';
-
-    const entryHTML = `
-            <strong>Name:</strong> <i>${entry.name}</i><br>
-            <strong>Level:</strong> <i>${entry.level}</i><br>
-            <strong>Keywords:</strong> <i>${entry.keywords}</i><br>
-        `;
-    listItem.innerHTML = entryHTML;
-
-    // Edit button event listener
-    const editButton = document.createElement('button');
-    editButton.className = 'btn btn-info btn-sm me-2';
-    editButton.textContent = 'Edit';
-
-    editButton.addEventListener('click', () => {
-        populateFormForEditing(entry);
-        listItem.remove();
-        tg.MainButton.color = '#229ED9';
-        tg.MainButton.setText("Save changes");
-        tg.MainButton.hide();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
+    function toggleDeleteButton(showButton) {
+        let deleteButton = document.getElementById("delete-button");
+        if (showButton) {
+            deleteButton.style.display = "block"; // Show the button
+        } else {
+            deleteButton.style.display = "none"; // Hide the button
         }
-    });
-
-    // Delete button event listener
-    const deleteButton = document.createElement('button');
-    deleteButton.className = 'btn btn-danger btn-sm me-2';
-    deleteButton.textContent = 'Delete';
-
-    deleteButton.addEventListener('click', () => {
-        listItem.remove();
-        if (!tg.MainButton.isVisible) {
-            tg.MainButton.show();
-            tg.MainButton.color = '#2CAB37';
-            tg.MainButton.setText("Save");
-        }
-    });
-
-    const div = document.createElement('div');
-    div.appendChild(editButton);
-    div.appendChild(deleteButton);
-    listItem.appendChild(div);
-    skillsList.appendChild(listItem);
-}
+    }
 
 // Function to populate the skill form with data for editing
 function populateFormForEditing(entry) {
-    document.getElementById('name').value = entry.name;
-    document.getElementById('level').value = entry.level;
-    document.getElementById('keywords').value = entry.keywords;
+    document.getElementById('id').value = entry.id || "";
+    document.getElementById('name').value = entry.name || "";
+    document.getElementById('level').value = entry.level || "";
+    document.getElementById('keywords').value = entry.keywords || "";
 }
 
-// Edit button event listener
-function editSkillEntry(entry) {
-    populateFormForEditing(entry);
-}
-
-// Add button click event listener
-function addEntry() {
-    if (validateInput(['name', 'level'])) {
-        return;
-    }
-    const entryData = {
-        name: document.getElementById('name').value,
-        level: document.getElementById('level').value,
-        keywords: document.getElementById('keywords').value,
-    };
-
-    addSkillEntry(entryData);
-    skillsForm.reset();
-};
+deleteButton.addEventListener("click", function () {
+            tg.showPopup({
+                              title: 'Action Delete',
+                              message: 'Are you sure you want to delete this skill?',
+                              buttons: [
+                                  {id: 'delete', type: 'destructive', text: 'Delete anyway'},
+                                  {type: 'cancel'},
+                              ]
+                          }, function(buttonId) {
+                              if (buttonId === 'delete') {
+                                 tg.sendData(JSON.stringify(
+                                 {
+                                     del_element: {
+                                         skill_id: document.getElementById('id').value
+                                     }
+                                 }
+                                 ));
+                                 tg.close();
+                              }
+                          });
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    if (tg.MainButton.text === "Add to List") {
-        addEntry();
-        tg.MainButton.color = '#2CAB37';
-        tg.MainButton.setText("Save");
-        return;
+   if (validateInput(['name', 'level'])) {
+           return;
     }
-    if (tg.MainButton.text === "Save changes") {
-        addEntry();
-        tg.MainButton.color = '#2CAB37';
-        tg.MainButton.setText("Save");
-        return;
-    }
-    const listItems = skillsList.querySelectorAll('li');
-    const sks = [];
-    listItems.forEach((item) => {
-        const strongElements = item.querySelectorAll('i');
-        sks.push({
-            name: strongElements[0].textContent.trim(),
-            level: strongElements[1].textContent.trim(),
-            keywords: strongElements[2].textContent.trim()
-        });
-    });
     tg.sendData(JSON.stringify({
-        skills: sks
+        skills: [{
+                id: document.getElementById('id').value,
+                name: document.getElementById('name').value,
+                level: document.getElementById('level').value,
+                keywords: document.getElementById('keywords').value,
+        }]
     }));
     tg.close();
 });
