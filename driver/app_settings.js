ec15621202af42c5c1a41ab6fe3fe8bfe48700a4
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-10-11 20:42:24 +0200

    without stringify just close telegram

---
+++ driver/app_settings.js
@@ -0,0 +1,49 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.setText("Сохранить");
+tg.MainButton.show();
+
+function getQueryParam(name) {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    return urlSearchParams.get(name);
+}
+
+const encodedJsonData = getQueryParam("json_data");
+
+document.getElementById('experience').addEventListener('input', function () {
+        const value = parseInt(this.value);
+        if (value < 1) {
+            this.value = '1';
+        } else if (value > 70) {
+            this.value = '70';
+        }
+    });
+
+if (encodedJsonData) {
+    const jsonData = decodeURIComponent(encodedJsonData);
+    const fixedJson = jsonData.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":');
+    const jsonObject = JSON.parse(fixedJson);
+    populateFormForEditing(jsonObject);
+}
+
+// Function to populate the award form with data for editing
+function populateFormForEditing(entry) {
+    document.getElementById('id').value = entry.id || "";
+    document.getElementById('username').value = entry.username || "";
+    document.getElementById('car').value = entry.car || "";
+    document.getElementById('experience').value = entry.experience || "";
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(JSON.stringify({
+        settings: {
+                     id: document.getElementById('id').value,
+                     username: document.getElementById('username').value,
+                     car: document.getElementById('car').value,
+                     experience: document.getElementById('experience').value
+        }
+    }));
+    tg.close();
+});
