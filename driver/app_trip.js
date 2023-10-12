6201807717109ef3d1a5c69b8dcbcbfc6579346d
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-10-12 12:02:05 +0200

    trip fully done

--- driver/app_trip.js
+++ driver/app_trip.js
@@ -5,7 +5,9 @@ tg.expand();
 tg.MainButton.setText("Добавить поездку");
 tg.MainButton.show();
 
-document.addEventListener("DOMContentLoaded", function () {
+let deleteButton = document.getElementById("delete-button");
+
+document.addEventListener("DOMContentLoaded", function() {
     const encodedJsonData = getQueryParam("json_data");
 
     if (encodedJsonData) {
@@ -15,18 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const jsonObject = JSON.parse(fixedJson);
         populateFormForEditing(jsonObject);
         toggleDeleteButton(true);
-        } else {
+    } else {
         toggleDeleteButton(false);
     }
 });
 
-document.getElementById('count').addEventListener('input', function () {
-        const value = parseInt(this.value);
-        if (value < 1) {
-            this.value = '1';
-        } else if (value > 10) {
-            this.value = '10';
-        }
+document.getElementById('count').addEventListener('input', function() {
+    const value = parseInt(this.value);
+    if (value < 1) {
+        this.value = '1';
+    } else if (value > 10) {
+        this.value = '10';
+    }
 });
 
 function getQueryParam(name) {
@@ -37,12 +39,11 @@ let initDataUnsafe = tg.initDataUnsafe;
 let userData = tg.initDataUnsafe.user;
 
 function toggleDeleteButton(showButton) {
-        let deleteButton = document.getElementById("delete-button");
-        if (showButton) {
-            deleteButton.style.display = "block"; // Show the button
-        } else {
-            deleteButton.style.display = "none"; // Hide the button
-        }
+    if (showButton) {
+        deleteButton.style.display = "block"; // Show the button
+    } else {
+        deleteButton.style.display = "none"; // Hide the button
+    }
 }
 
 
@@ -64,11 +65,35 @@ function populateFormForEditing(entry) {
     document.getElementById('currency').value = entry.currency || "";
 }
 
+deleteButton.addEventListener("click", function() {
+    tg.showPopup({
+        title: 'Удаление',
+        message: 'Вы уверены, что хотите удалить поездку?',
+        buttons: [{
+            id: 'delete',
+            type: 'destructive',
+            text: 'Конечно удалить'
+        }, {
+            type: 'cancel',
+            text: 'Нет передумал'
+        }, ]
+    }, function(buttonId) {
+        if (buttonId === 'delete') {
+            tg.sendData(JSON.stringify({
+                del_element: {
+                    trip_id: document.getElementById('id').value
+                }
+            }));
+            tg.close();
+        }
+    });
+});
+
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    if (validateInput(['dDate', 'price','count'])) {
+    if (validateInput(['dDate', 'price', 'count'])) {
         return;
     }
-    if(encodedJsonData) {
+    if (encodedJsonData) {
         update();
     } else {
         save();
@@ -77,33 +102,24 @@ Telegram.WebApp.onEvent("mainButtonClicked", function() {
 
 // Function to populate the award form with data for editing
 function update() {
-    document.getElementById('id').value = entry.id || "";
-    document.getElementById('dCountry').value = entry.dCountry || "";
-    document.getElementById('dCity').value = entry.dCity || "";
-    document.getElementById('aCountry').value = entry.aCountry || "";
-    document.getElementById('aCity').value = entry.aCity || "";
-    document.getElementById('dDate').value = entry.dDate || "";
-    document.getElementById('price').value = entry.price || "";
-    document.getElementById('count').value = entry.count || "";
-    document.getElementById('currency').value = entry.currency || "";
     tg.sendData(JSON.stringify({
-            trip: {
-                id: document.getElementById('id').value,
-                dCountry: document.getElementById('dCountry').value,
-                dCity: document.getElementById('dCity').value,
-                aCountry: document.getElementById('aCountry').value,
-                aCity: document.getElementById('aCity').value,
-                dDate: document.getElementById('dDate').value,
-                price: document.getElementById('price').value,
-                count: document.getElementById('count').value,
-                currency: document.getElementById('currency').value
-            }
+        trip: {
+            id: document.getElementById('id').value,
+            dCountry: document.getElementById('dCountry').value,
+            dCity: document.getElementById('dCity').value,
+            aCountry: document.getElementById('aCountry').value,
+            aCity: document.getElementById('aCity').value,
+            dDate: document.getElementById('dDate').value,
+            price: document.getElementById('price').value,
+            count: document.getElementById('count').value,
+            currency: document.getElementById('currency').value
+        }
     }));
     tg.close();
 }
 
 function save() {
-tg.showPopup({
+    tg.showPopup({
         title: 'Сохранение поездки',
         message: 'Вы уверены в правильности заполнении информации о поездке?',
         buttons: [{
@@ -124,7 +140,7 @@ tg.showPopup({
                 price: document.getElementById('price').value,
                 count: document.getElementById('count').value
             };
-            fetch('https://httpbin.org/post?userId='+`${userData.id }`, {
+            fetch('https://httpbin.org/post?userId=' + `${userData.id }`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
