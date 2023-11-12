572a03123e17c6534b7cde76883d8f8411292b3a
Andrei Kosintsev <andrei.kosintsau@gmail.com>
2023-11-12 14:27:53 +0100

    new ui

--- driver/scripts/app_filter.js
+++ driver/scripts/app_filter.js
@@ -6,14 +6,25 @@ tg.MainButton.setText("Добавить фильтр");
 tg.MainButton.show();
 
 let deleteButton = document.getElementById("delete-button");
+let savingSpinner = document.createElement("div");
+savingSpinner.innerHTML = '<i class="fas fa-spinner fa-spin"></i> Saving';
+savingSpinner.style.display = "none";
+savingSpinner.style.position = "absolute";
+savingSpinner.style.top = "50%";
+savingSpinner.style.left = "50%";
+savingSpinner.style.transform = "translate(-50%, -50%)";
+savingSpinner.style.zIndex = "3";
+document.body.appendChild(savingSpinner);
 
 const tokenJson = getQueryParam("token");
-if(tokenJson) {
-        const decodedToken = decodeURIComponent(tokenJson);
-        const fixedTokenJson = decodedToken.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":');
-        const tokenObject = JSON.parse(fixedTokenJson);
-        token = btoa([tokenObject.userId,tokenObject.id,tg.initDataUnsafe.query_id].join(' '));
-        document.getElementById('token').value = token;
+let token = "";
+
+if (tokenJson) {
+    const decodedToken = decodeURIComponent(tokenJson);
+    const fixedTokenJson = decodedToken.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2":');
+    const tokenObject = JSON.parse(fixedTokenJson);
+    token = btoa([tokenObject.userId, tokenObject.id, tg.initDataUnsafe.query_id].join(' '));
+    document.getElementById('token').value = token;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -45,7 +56,6 @@ function toggleDeleteButton(showButton) {
         deleteButton.style.display = "none"; // Hide the button
     }
 }
-
 
 // Function to populate the award form with data for editing
 function populateFormForEditing(entry) {
@@ -87,36 +97,49 @@ deleteButton.addEventListener("click", function() {
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    toggleSavingSpinner(true);
     tg.showPopup({
-            title: 'Сохранение фильтра',
-            message: 'Вы уверены в правильности заполнении информации?',
-            buttons: [{
-                id: 'delete',
-                type: 'destructive',
-                text: 'Сохранить'
-            }, {
-                type: 'cancel'
-            }, ]
-        }, function(buttonId) {
-            if (buttonId === 'delete') {
-                const data = {
-                    dCountry: document.getElementById('dCountry').value,
-                    dCity: document.getElementById('dCity').value,
-                    aCountry: document.getElementById('aCountry').value,
-                    aCity: document.getElementById('aCity').value,
-                    dDate: document.getElementById('dDate').value,
-                    token: document.getElementById('token').value
-                };
-                fetch('https://tdriver-service.kvadsoft.de/create', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify(data)
-                    }).then(response => response.json())
-                    .then(responseData => {
-                        tg.close();
-                    }).catch(error => {});
-            }
-        });
+        title: 'Сохранение фильтра',
+        message: 'Вы уверены в правильности заполнении информации?',
+        buttons: [{
+            id: 'delete',
+            type: 'destructive',
+            text: 'Сохранить'
+        }, {
+            type: 'cancel'
+        }, ]
+    }, function (buttonId) {
+        toggleSavingSpinner(false);
+
+        if (buttonId === 'delete') {
+            const data = {
+                dCountry: document.getElementById('dCountry').value,
+                dCity: document.getElementById('dCity').value,
+                aCountry: document.getElementById('aCountry').value,
+                aCity: document.getElementById('aCity').value,
+                dDate: document.getElementById('dDate').value,
+                token: document.getElementById('token').value
+            };
+            fetch('https://tdriver-service.kvadsoft.de/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            }).then(response => response.json())
+                .then(responseData => {
+                    tg.close();
+                }).catch(error => {
+                    // Handle error
+                });
+        }
+    });
 });
+
+function toggleSavingSpinner(showSpinner) {
+    if (showSpinner) {
+        savingSpinner.style.display = "block"; // Show the spinner
+    } else {
+        savingSpinner.style.display = "none"; // Hide the spinner
+    }
+}
