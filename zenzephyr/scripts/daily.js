df7eb80388c2925b39150090e6c79e778e40fa1b
andrekosintsev <andrei.kosintsau@gmail.com>
2024-08-13 13:36:01 +0200

    free image for daily

--- zenzephyr/scripts/daily.js
+++ zenzephyr/scripts/daily.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 function populateFormForEditing(entry) {
     if(entry.image) {
         //document.getElementById("image").style.backgroundImage = `url('${entry.image}')`;
-        document.getElementById("image").style.backgroundImage = "url('https://zira.uz/wp-content/uploads/2023/02/zhayma-1.jpg')";
+        document.getElementById("image").style.backgroundImage = "url('https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=2428&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
 
     }
     if(entry.ingredientLines) {
