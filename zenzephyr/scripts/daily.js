dbbe6297917f4b6f966f70510e447f5989208617
andrekosintsev <andrei.kosintsau@gmail.com>
2024-08-13 14:12:39 +0200

    free image for daily

--- zenzephyr/scripts/daily.js
+++ zenzephyr/scripts/daily.js
@@ -19,10 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function populateFormForEditing(entry) {
+document.getElementById("image").style.backgroundImage = "url('https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=2428&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
     if(entry.image) {
-        //document.getElementById("image").style.backgroundImage = `url('${entry.image}')`;
-        document.getElementById("image").style.backgroundImage = "url('https://images.unsplash.com/photo-1466637574441-749b8f19452f?q=80&w=2428&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
-
+        document.getElementById("image").style.backgroundImage = `url('${entry.image}')`;
     }
     if(entry.ingredientLines) {
      const ul = document.createElement("ul");
